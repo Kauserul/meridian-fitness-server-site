3011be22fd04e6f1c485befad2a9647f4663e47a
Kauserul <kauserulislam0055@gmail.com>
2022-11-08 02:45:52 +0600

    get services added

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = 'mongodb://localhost:27017'
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mcdvihz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollection = client.db('cookups').collection('services')
+
+        app.get('/services', async(req, res) =>{
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
+
 
 app.get('/', (req, res) =>{
     res.send('API Running...')
