d0da81b651968ca19034621e23f0f3502fc334a7
Kauserul <kauserulislam0055@gmail.com>
2022-11-10 01:36:04 +0600

    services all api done

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ async function run(){
             res.send(result)
         })
 
+        app.get('/servicesAll', async(req, res) =>{
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
         app.get('/services/:id', async(req, res) =>{
             const id = req.params.id;
             const query = {_id : ObjectId(id)}
