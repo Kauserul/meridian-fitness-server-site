1f3044be397cfb3b5fb62820d816352bccb1657b
Kauserul <kauserulislam0055@gmail.com>
2022-11-10 21:00:16 +0600

    almost done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
@@ -12,11 +13,35 @@ app.use(express.json())
 // const uri = 'mongodb://localhost:27017'
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mcdvihz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri)
+
+function veryflyJWT(req, res, next){
+    const authHeader = req.headers.authorization
+    
+    if(!authHeader){
+        return res.status(401).send({message : "unauthorized access"})
+    }
+
+    const token = authHeader.split(' ')[1]
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+        if(err){
+            return res.status(403).send({message : "Forbidden access"})
+        }
+        req.decoded = decoded
+        next()
+    })
+}
+
 async function run(){
     try{
         const serviceCollection = client.db('cookups').collection('services')
         const reviewCollection = client.db('cookups').collection('reviews')
+
+        app.post('/jwt', async(req, res) =>{
+            const user = req.body;
+            const token= jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn : '1d'})
+            res.send({token})
+        })
 
         app.get('/services', async(req, res) =>{
             const query = {}
@@ -48,7 +73,11 @@ async function run(){
 
 
         // review api
-        app.get('/myReview', async(req, res) =>{
+        app.get('/myReview', veryflyJWT, async(req, res) =>{
+            const decoded = req.decoded
+            if(decoded.email !== req.query.email){
+                return res.status(401).send({message : "unauthorized access"})
+            }
             let query = {}
             if(req.query.email){
                 query = {
@@ -61,8 +90,10 @@ async function run(){
 
         })
 
-        app.get('/review', async(req, res) =>{
-            const query = {}
+        app.get('/review/:id', async(req, res) =>{
+            const id = req.params.id
+            // console.log(id)
+            const query = {serviceId :(id)}
             const cursor = reviewCollection.find(query)
             const review = await cursor.toArray()
             res.send(review)
@@ -81,6 +112,8 @@ async function run(){
             const result = await reviewCollection.deleteOne(query)
             res.send(result)
         })
+
+
     }
     finally{
 
